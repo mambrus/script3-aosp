aa9a7f3124f3f2b36a2ca099e7ab98c9c4f0b512
Michael Ambrus <michael.ambrus@gmail.com>
2011-02-19 01:13:23 +0100

    Added initial structure for aosp

---
+++ apk/restore.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Install all the apk:s that can be found in this dir.
+# -r is undokumented. It overwrites previous instalations
+
+echo wait-for-device
+#adb wait-for-device root
+adb wait-for-device
+  for a in *.apk ; do
+	echo "Installing: [$a]"
+	adb wait-for-device install -r $a | tee -a apk.log | grep "Failure"
+	if [ $? -eq 0 ]; then
+	  echo
+	  echo "==============================================="
+	  adb shell df
+	  echo "==============================================="
+	  exit 1
+	fi
+  done 
+
+
+
+
+
+
