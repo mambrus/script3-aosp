480ac46f021250f42519f31781a6f8cf9ed8fc3b
Michael Ambrus <michael.ambrus@sonyericsson.com>
2011-02-25 13:55:04 +0100

    Adding script for easily get last Android build source

---
+++ landroid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2011-02-25
+
+if [ -z $LANDROID_SH ]; then
+
+LANDROID_SH="landroid.sh"
+
+# This script returns the full path of the last successfully built Android
+# source path.
+# It relies on that your build updates the file ~/.landroid
+function landroid() {
+    cat ~/.landroid  | egrep '^/' | tail -n1
+}
+
+set -e
+source s3.ebasename.sh
+if [ "$LANDROID_SH" == $( ebasename $0 ) ]; then
+	#Not sourced, do something with this.
+	landroid $@
+	exit $?
+fi
+
+fi
