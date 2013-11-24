d4f283e4bb27fececf833a6adfffdcee023014d5
Michael Ambrus <michael.ambrus@gmail.com>
2013-11-24 21:41:41 +0100

    Adapted to s3 standard
    * Script forever.logcat.sh adapted to follow s3 standard code-convention.
    * logsim.sh added. A log-simulator attempt. A much better simulator is HEMUL.
    
    Change-Id: I70ab73c1f27071c2a73c0ac17ee604c3b4143861

--- forever/logcat.sh
+++ forever/logcat.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
-set -u
-set -e
-for (( ; 1 ; )); do
-  echo "wait-for-device"
-  adb wait-for-device
-  TS=$(date "+%y%m%d_%H%M%S");
-  echo "Starting to log in file: logcat_${TS}"
-  #adb logcat -v time | tee "logcat_${TS}".txt;
-  adb logcat -v thread | tee "logcat_${TS}".txt;
-  echo
-  echo
-  echo "Finished logging at $(date "+%y%m%d_%H%M%S"). Data is in: logcat_${TS}.txt"
-done
+
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2013-04-26
+
+if [ -z $LOGCAT_SH ]; then
+
+LOGCAT_SH="logcat.sh"
+
+function logcat() {
+	for (( ; 1 ; )); do
+		echo "wait-for-device"
+		adb wait-for-device
+		TS=$(date "+%y%m%d_%H%M%S");
+		echo "Starting to log in file: logcat_${TS}"
+		adb logcat -v threadtime | tee "logcat_${TS}".txt;
+		echo
+		echo
+		echo "Finished logging at $(date "+%y%m%d_%H%M%S"). Data is in: logcat_${TS}.txt"
+	done
+}
+
+source s3.ebasename.sh
+if [ "$LOGCAT_SH" == $( ebasename $0 ) ]; then
+	#Not sourced, do something with this.
+
+	LOGCAT_SH_INFO=${LOGCAT_SH}
+	source .aosp.forever.ui..logcat.sh
+
+	logcat "$@"
+	RC=$?
+
+	exit $RC
+fi
+
+fi
--- logsim.sh
+++ logsim.sh
@@ -7,10 +7,22 @@ if [ -z $LOGSIM_SH ]; then
 
 LOGSIM_SH="logsim.sh"
 
+#Precision sleep
+function psleep() {
+	python -c "from time import sleep; sleep($1)"
+}
+
+function cond_sleep() {
+	read line
+
+	echo -n $line
+}
+
 function logsim() {
-		cat -- | \
-		sed -e 's/\x1b.\x4b//g' | \
-		sed -e 's/\x0d/\n/g'
+		cat ${INFILE} | awk -v ST="0.1" '{print $0; system("sleep "ST"")}'  >> ${OUTFILE}
+		#cat ${INFILE} | cond_sleep >> ${OUTFILE}
+		#cat ${INFILE} >> ${OUTFILE}
+		#echo "hej på dig"  >> ${OUTFILE}
 }
 
 source s3.ebasename.sh
@@ -19,6 +31,19 @@ if [ "$LOGSIM_SH" == $( ebasename $0 ) ]; then
 
 	LOGSIM_SH_INFO=${LOGSIM_SH}
 	source .aosp.ui..logsim.sh
+	set -u
+
+	if [ "X${PIPE}" != "X" ]; then
+		OUTFILE="${PIPE}"
+
+		if [ -a "${OUTFILE}" ] && [ ! -p "${OUTFILE}" ]; then
+			echo "Error: File [${OUTFILE}] already exists but it's not a pipe"
+			exit 1
+		fi
+		if [ ! -p "${OUTFILE}" ]; then
+			mkfifo "${OUTFILE}"
+		fi
+	fi
 
 	logsim "$@"
 	RC=$?
